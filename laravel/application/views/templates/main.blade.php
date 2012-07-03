c4d3b973f386602c4477003120d7a5eceb4c5d0e
Andrew Morrison <achmorrison@gmail.com>
2012-07-03 13:53:06 -0500

    Add link to main template for all problems.

--- laravel/application/views/templates/main.blade.php
+++ laravel/application/views/templates/main.blade.php
@@ -54,6 +54,7 @@
 <li><a href="{{URL::to_action('problems@mine')}}"><i class="icon-list"></i> View my problems</a></li>
 <li><a href="{{URL::to_action('tags@browsetags')}}"><i class="icon-list"></i> Browse tags</a></li>
 <li><a href="{{URL::to_action('problems@last', array(10))}}"><i class="icon-list"></i> Last 10 problems</a></li>
+<li><a href="{{URL::to_action('problems@all')}}"><i class="icon-list"></i> All problems</a></li>
 @endif
 @yield_section
 </ul>
