6272f1e1574ee25bf996a1718cfc26d7db2e7b78
John Burk <john.burk98@gmail.com>
2012-07-15 20:34:10 -0400

    answers now show

--- laravel/application/config/database.php
+++ laravel/application/config/database.php
@@ -72,7 +72,7 @@ return array(
 			'host'     => 'localhost',
 			'database' => 'mydb',
 			'username' => 'root',
-			'password' => '',
+			'password' => 'root',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 		),
