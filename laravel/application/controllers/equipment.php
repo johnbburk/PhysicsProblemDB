964868a356a7e758f901fbbcc6a3685531d18744
Andy Rundquist <andy.rundquist@gmail.com>
2012-08-20 22:44:21 -0500

    equipment without login

--- laravel/application/controllers/equipment.php
+++ laravel/application/controllers/equipment.php
@@ -4,10 +4,7 @@ class Equipment_Controller extends Base_Controller
 {
 	public $restful=true;
 	
-	public function __construct()
-	{
-		$this->filter('before', 'auth');
-	}
+	
 	
 	public function get_index()
 	{
